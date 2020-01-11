60f5fae1fdf4ae55a14845952f27ef370110e404
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-01-11 22:53:50 +0200

    dev: PieceTable

---
+++ source/data-structures/PieceTable/__specs__/index.ts
@@ -0,0 +1,27 @@
+import PieceTable from '../';
+
+
+
+describe('PieceTable', () => {
+    it('basic test - insert()', () => {
+        const pieceTable = new PieceTable('original string');
+        pieceTable.insert(' text', 15);
+        const text = pieceTable.stringAt(0, 20);
+        expect(text).toBe('original string text');
+    });
+
+    it('basic test - delete()', () => {
+        const pieceTable = new PieceTable('original string');
+        pieceTable.insert(' text', 15);
+        pieceTable.delete(8, 1);
+        const text = pieceTable.stringAt(0, 19);
+        expect(text).toBe('originalstring text');
+    });
+
+    it('basic test - getSequence()', () => {
+        const pieceTable = new PieceTable('original string');
+        pieceTable.insert('\ntext', 15);
+        const text = pieceTable.getSequence();
+        expect(text).toBe('original string\ntext');
+    });
+});
