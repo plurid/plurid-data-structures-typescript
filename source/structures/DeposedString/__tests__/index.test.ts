4eae065cec314511ebbc0d6cde1f7b02117909f9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-10-29 18:44:46 +0300

    test: DeposedString

--- source/structures/DeposedString/__tests__/index.test.ts
+++ source/structures/DeposedString/__tests__/index.test.ts
@@ -1,6 +1,10 @@
 // #region imports
     // #region external
     import DeposedString from '../';
+
+    import {
+        DeposedStringStages,
+    } from '~data/interfaces';
     // #endregion external
 // #endregion imports
 
@@ -14,6 +18,26 @@ describe('DeposedString', () => {
         deposedString.push('a');
         deposedString.push('ab');
         deposedString.push('abc');
+
+        const a = deposedString.get(0);
+        expect(a).toEqual('a');
+
+        const ab = deposedString.get(1);
+        expect(ab).toEqual('ab');
+
+        const abc = deposedString.get(2);
+        expect(abc).toEqual('abc');
+    });
+
+    it('loads', () => {
+        const data: DeposedStringStages = [
+            [["+",0,"a"]],
+            [["+",1,"b"]],
+            [["+",2,"c"]],
+        ];
+
+        const deposedString = new DeposedString('');
+        deposedString.load(data);
 
         const a = deposedString.get(0);
         expect(a).toEqual('a');
